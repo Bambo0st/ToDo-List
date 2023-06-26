a24ed4b5292be8ac4feaf1af315ccc2829325fe1
Bambo0st <99262296+Bambo0st@users.noreply.github.com>
2023-06-26 07:55:34 +0000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,6 +18,7 @@ function App() {
   }
   return (
     <div>
+      <h1>IamgFod</h1>
       <Header />
       <CreateArea onAdd={addItem} />
       <Note key={1} title="Note title" content="Note content" />
